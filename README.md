4e4f8b60479187fa32e4a18deaed0232889169bd
Alexander <25225552+alexander-lindner@users.noreply.github.com>
2020-12-27 17:49:27 +0100

    Added github actions files

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 ./build.sh
 docker run -ti --rm --volume $(pwd):/build -w /build alexanderlindner/cish:latest bash
 ./build.cish
+
+
+docker build -t alexanderlindner/cish:latest docker/build
+
+#echo '#!/usr/bin/java --source 12 -cp target/ci-interpreter-0.1-SNAPSHOT-jar-with-dependencies.jar' >$INTERPRETER
+#cat <<EOF >>$INTERPRETER
+#import java.io.*;
+#public class Wrapper {
+#  public static void main(final String... args) throws IOException {
+#    new org.alindner.cish.interpreter.Interpreter(args);
+#  }
+#}
+#EOF
+#chmod +x $INTERPRETER
 ```
--- build.sh
+++ build.sh
@@ -5,20 +5,8 @@ mvn clean process-resources package
 if [ -f $INTERPRETER ]; then
   rm $INTERPRETER
 fi
-#
+
 echo '#!/usr/bin/java -jar ' >$INTERPRETER
 cat interpreter/target/interpreter-0.1-SNAPSHOT-jar-with-dependencies.jar >>$INTERPRETER
 chmod +x $INTERPRETER
 cp $INTERPRETER docker/build
-docker build -t alexanderlindner/cish:latest docker/build
-
-#echo '#!/usr/bin/java --source 12 -cp target/ci-interpreter-0.1-SNAPSHOT-jar-with-dependencies.jar' >$INTERPRETER
-#cat <<EOF >>$INTERPRETER
-#import java.io.*;
-#public class Wrapper {
-#  public static void main(final String... args) throws IOException {
-#    new org.alindner.cish.interpreter.Interpreter(args);
-#  }
-#}
-#EOF
-#chmod +x $INTERPRETER
