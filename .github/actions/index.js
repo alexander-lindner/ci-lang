368eef435c939848507e4c3389c4627a7e03781e
Alexander <25225552+alexander-lindner@users.noreply.github.com>
2020-12-29 20:13:55 +0100

    added action for cish binary

---
+++ .github/actions/index.js
@@ -0,0 +1,69 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const http = require('@actions/http-client');
+
+const fs = require('fs');
+
+function copy(callback) {
+    let oldPath = "cish",
+        newPath = "/bin/cish",
+        readStream = fs.createReadStream(oldPath),
+        writeStream = fs.createWriteStream(newPath);
+    readStream.on('error', callback);
+    writeStream.on('error', callback);
+
+    readStream.on('close', () => fs.unlink(oldPath, callback));
+
+    readStream.pipe(writeStream);
+}
+
+async function downloadFile(version, callback) {
+    const file = fs.createWriteStream("cish");
+
+    const http = new http.HttpClient('actions-cish', undefined, {
+        allowRetries: true,
+        maxRetries: 3
+    });
+    const url = `https://github.com/alexander-lindner/cish/releases/download/${version}/cish`;
+    core.debug(`Downloading from ${url}`);
+    const response = await http.get(url);
+    const statusCode = response.message.statusCode || 0;
+
+    if (statusCode < 200 || statusCode > 299) {
+        const message = `Unexpected HTTP status code '${response.message.statusCode}' when retrieving versions from '${url}'. ${body}`.trim();
+        throw new Error(message);
+    } else {
+        response.message.pipe(file).on('close', () => {
+            file.close();
+            fs.rename("cish", "/bin/cish", (err) => {
+                if (err) {
+                    if (err.code === 'EXDEV') {
+                        copy(callback);
+                    } else {
+                        callback(err);
+                    }
+                    return;
+                }
+                callback();
+            });
+        })
+    }
+}
+
+try {
+    const version = core.getInput('version');
+    core.debug(`Download Version ${version}!`);
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    core.debug(`The event payload: ${payload}`);
+
+    let callback = (err) => {
+        if (err) {
+            throw  err;
+        }
+        core.debug("Cish successfully installed");
+    };
+    downloadFile("v0.3.0", callback);
+} catch (error) {
+    core.setFailed(error.message);
+}
