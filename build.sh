88a11bd0765f7b31cfe102093da18ea911ad94b5
Alexander Lindner <25225552+alexander-lindner@users.noreply.github.com>
2021-01-29 15:19:31 +0200

    Fixed a bug for versioning.

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+if [[ "$GITHUB_REF" == refs/heads/* ]]; then
+  export VERSIONING_GIT_BRANCH=${GITHUB_REF#refs/heads/}
+elif [[ "$GITHUB_REF" == refs/tags/* ]]; then
+  export VERSIONING_GIT_TAG=${GITHUB_REF#refs/tags/}
+elif [[ "$GITHUB_REF" == refs/pull/*/merge ]]; then
+  export VERSIONING_GIT_BRANCH=${GITHUB_REF#refs/}
+  VERSIONING_GIT_BRANCH=${VERSIONING_GIT_BRANCH%/merge}
+fi
+
 INTERPRETER="target/cish"
 mvn clean process-resources package
 
