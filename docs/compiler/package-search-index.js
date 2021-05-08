86194028955e5f65c0c5b0d760e9d18fb7341c52
alexander-lindner <25225552+alexander-lindner@users.noreply.github.com>
2021-05-08 12:56:28 +0000

    Deploying to gh-pages from @ alexander-lindner/cish@5cce7a19ee88140362d1b5645bb7875ed941dcb5 🚀

--- docs/compiler/package-search-index.js
+++ docs/compiler/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.alindner.cish.compiler"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.alindner.cish.compiler","l":"org.alindner.cish.compiler"},{"m":"org.alindner.cish.compiler","l":"org.alindner.cish.compiler.exceptions"},{"m":"org.alindner.cish.compiler","l":"org.alindner.cish.compiler.utils"}];updateSearchResults();
